9e0970b3f53ba8775ef4677d68c3d5b552e0de7e
Atul <atul.r20@iiits.in>
2023-05-20 14:06:08 +0530

    new commit

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
 import Projectpg from '../components/projectpg'
-import PricingCard from '../components/PricingCard'
+// import PricingCard from '../components/PricingCard'
 import Work from '../components/Work'
 
 const Project = () => {
